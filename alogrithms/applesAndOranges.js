38acfc59327fd32b171a8278ebf0e32c115ad934
Roman Navarro <ronnoverro@gmail.com>
2022-08-21 22:15:13 -0700

    applesAndOranges phase 2

--- alogrithms/applesAndOranges.js
+++ alogrithms/applesAndOranges.js
@@ -6,24 +6,27 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
   // Write your code here
   // const range = (start, stop) => Array.from({ length: stop - start + 1 }, (_, i) => start + i)
   // let housePoints = range(s, t);         // [ 7, 8, 9, 10, 11 ]   GOOD  
-  let housePoints = (s, t) => Array.from({ length: t - s + 1 }, (_, i) => s + i)
+  let housePoints = Array.from({ length: t - s + 1 }, (_, i) => s + i)
 
-
-  // const targetFruit = (array, tree) => array.map(f => tree + f).filter(f => housePoints.includes(f)).length
+  //const targetFruit = (array, tree) => array.map(f => tree + f).filter(f => housePoints.includes(f)).length
+  function targetFruit(array, tree) {
+    return array.map(f => tree + f).filter(f => housePoints.includes(f)).length
+  }
+  
   // let aPositions = targetFruit(apples, a)
   // let oPositions = targetFruit(oranges, b)
 
-  let aPositions = apples.map(apple => a + apple).filter(apple => housePoints.includes(apple)).length         
-  // [ 3, 7, 6 ]
-  let oPositions = oranges.map(orange => b + orange).filter(orange => housePoints.includes(orange)).length
-  // [ 20, 9 ]
-
-  console.log(`${aPositions}\n${oPositions}`)   
+  // console.log(`${aPositions}\n${oPositions}`) 
+  console.log(targetFruit(apples, a), targetFruit(oranges, b))  
+  // console.log(housePoints)
 }
 
 countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
 // OPTIMIZE THIS CRAP
+// APPERENTLY "FILTER" IS SLOWER THAN OLD FASHIONED FOR LOOP
 
+// CASE 10 IS INSANE. MILLIONS OF LARGE NUMBERS TO COMPUTE
+// TODO: USE MEMOIZATION. NOPE, CANT DO THAT. NEED TO ASSUME ALL THE NUMS ARE UNIQUE
 
 /*  
   * The function accepts following parameters:
@@ -34,3 +37,9 @@ countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
   *  5. INTEGER_ARRAY apples
   *  6. INTEGER_ARRAY oranges
 */
+
+
+// let aPositions = apples.map(apple => a + apple).filter(apple => housePoints.includes(apple)).length         
+// // [ 3, 7, 6 ]
+// let oPositions = oranges.map(orange => b + orange).filter(orange => housePoints.includes(orange)).length
+// [ 20, 9 ]
