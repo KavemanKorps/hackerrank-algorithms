b3aec79dc2bd443e2ab87c4bebf89492ee33a087
Roman Navarro <ronnoverro@gmail.com>
2022-08-24 14:34:49 -0700

    breaking records

---
+++ alogrithms/subarrayDivision.js
@@ -0,0 +1,9 @@
+
+// ([1 1 1 1 1 1], 3, 2) -> 0
+// s = array, d (what it should equal), m (number of nums to add) 
+
+function birthday(s, d, m) {
+    // Write your code here
+    let numRemaining = m;
+    let goodNums = [];          // can only be 'm' nums long  
+}
