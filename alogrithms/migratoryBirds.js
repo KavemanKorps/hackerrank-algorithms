a3d1308db647b1e89fb389cd9dc29e69f6a340a9
Roman Navarro <ronnoverro@gmail.com>
2022-08-27 18:15:04 -0700

    migratoryBirds

---
+++ alogrithms/migratoryBirds.js
@@ -0,0 +1,14 @@
+function migratoryBirds(arr) {
+  let uniques = {};
+  let currMax = 0;
+  let maxys = [];
+
+  arr.map(i => uniques.includes(i) ? "" : uniques.i = 0)
+    .map(i => uniques.i += 1);
+
+    const maxValues = Math.max(...Object.values(uniques));
+    for (let pair in uniques) {
+    if (pair === maxValue) maxys.push(pair)
+    }
+    return Math.min(maxys);
+}
