98a334ffbda5173bc78a54ff028cb2f3efc5808d
Roman Navarro <ronnoverro@gmail.com>
2022-09-12 22:08:35 -0700

    catsAndMouse done

--- alogrithms/catsAndMouse2.js
+++ alogrithms/catsAndMouse2.js
@@ -1,14 +1,16 @@
 function catAndMouse(x, y, z) {
   let cats = {"Cat A": x, "Cat B": y};
-  Object.values(cats).forEach(v =>  v = Math.abs(v - z));
+  // Object.values(cats).forEach(v =>  v = Math.abs(v - z));
 
+  Object.keys(cats).forEach(key => cats[key] = Math.abs(cats[key] - z));    
+  // subtract cat distance from mouse distance, turn to positive number if negative 
 
-  Object.keys(cats).forEach(key =>  cats[key] = Math.abs(cats[key] - z));
-  let min = Object.entries(cats).reduce((min, entry) => entry[1] >= min[1] ? entry: min, [0, +Infinity]);
-
-  return min;
-  //return cats;
+  if (cats["Cat A"] === cats["Cat B"]) {
+    return "Mouse C"
+  }
+  return Object.entries(cats).reduce((min, entry) => entry[1] <= min[1] ? entry : min, [0, +Infinity])[0];
 }
 
 
-console.log(catAndMouse(2, 5, 4));      // Cat B
+console.log(catAndMouse(2, 5, 4));      // Cat B, 2 - 4 = -2 (2), 5 - 4 = (1)
+console.log(catAndMouse(1, 3, 2));
