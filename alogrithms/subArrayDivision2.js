950e97d8d5a3457abfa5be9c630005ca3cf1837e
Roman Navarro <ronnoverro@gmail.com>
2022-10-19 22:01:47 -0700

    subArrayDiv almost done

--- alogrithms/subArrayDivision2.js
+++ alogrithms/subArrayDivision2.js
@@ -7,25 +7,20 @@ function birthday(s, d, m) {
   let sCopy = [...s];
   let currArr = [];
   for (let i = 0; i < sCopy.length; i++) {
-    if (currArr.length < m) {
-      currArr.push(s[i])
-      if (currArr.reduce((a, b) => a + b) < d) {
+    for (let j = 0; j < s.length; j++) {
+      if (s[i] <= d) currArr.push(s[i]);    // INITIAL STEP
 
+      if (currArr.length < m) {
+        () => i !== j ? currArr.push(s[j]) : null
+      }
+
+      else {
+        arrs.push(currArr);
+        currArr = [];
       }
     }
-
-    // if (currArr.length) {
-    //   if (currArr.reduce((a, b) => a + b) < d && currArr.length < m) {
-    //     currArr.push(s[i])
-    //   }
-    //   else {
-    //     console.log(currArr);
-    //     arrs.push(...currArr);
-    //     currArr = [];     // reset currArr
-    //   }
-    // } 
   }
-  //console.log(arrs)
+  console.log(arrs)
 }
 
-birthday([1, 2, 1, 3, 2], 3, 2);
+birthday([1, 2, 1, 3, 2], 3, 2);    // -> [1, 2], [1, 2] -> 2
