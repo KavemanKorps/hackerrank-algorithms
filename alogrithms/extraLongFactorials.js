c4634ef56f748f8c003ca6d34a9ac2a6d9ca81b2
Roman Navarro <ronnoverro@gmail.com>
2022-10-22 14:20:23 -0700

    drawingBook finally done

--- alogrithms/extraLongFactorials.js
+++ alogrithms/extraLongFactorials.js
@@ -1,7 +1,13 @@
 function extraLongFactorials(n) {
   // Write your code here
-  let numArr = Array.from({ length: n - 1 + 1 }, (_, x) => 1 + x).reverse();
-  console.log(BigInt(numArr.reduce((a, b) => a * b)));
+  let numArr = Array.from({ length: n - 1 + 1 }, (_, x) => 1 + x).reverse(); // range arr
+  console.log(numArr.reduce((a, b) => a * b).toLocaleString("fullwide", {useGrouping:false}));
 }
 
-extraLongFactorials(25);
+extraLongFactorials(25)
+//console.log((1.5511210043330984e+25).toLocaleString("fullwide", {useGrouping:false}));
+
+// 1.5511210043330984e+25 -> 1.551121e+25 -> 15511210043330985984000000
+
+// 15511210043330983907819520
+//15511210043330984000000000
