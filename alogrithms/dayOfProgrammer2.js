31b05a997f4cbd8f5b70735bc7d6c0b7410d8350
Roman Navarro <ronnoverro@gmail.com>
2022-10-25 12:25:32 -0700

    dayOfProgrammer finally done

--- alogrithms/dayOfProgrammer2.js
+++ alogrithms/dayOfProgrammer2.js
@@ -8,14 +8,11 @@ function dayOfProgrammer(year) {
     if (year % 4 === 0) {   // leap year, so feb has 29 days
       months[2] = 29
     }
-    else {
-      date = `12.09.${year}` // this minus 13 is 1918 date
-    }
   }
+  
   if (year === 1918) {       // YEAR 1918
     months[2] = 17           // feb starts at 14th, leaving 17 days 
   }
-
   else {                     // GREGARIAN 
     if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
       months[2] = 29
@@ -30,7 +27,7 @@ dayOfProgrammer(1800);    // FEB 29, 12.09.1800
 dayOfProgrammer(2016);    // FEB 29, 12.09.2016
 dayOfProgrammer(2017);    // FEB 28, 13.09.2017
 dayOfProgrammer(1918);    // FEB 28, 26.09.1918
-dayOfProgrammer(1917);    // 13.09.1917
+dayOfProgrammer(1917);    // 13.09.1917, correct: 13.09.1917
 dayOfProgrammer(1741);    // 13.09.1741
-
+dayOfProgrammer(1915);    // 13.09.1915, correct: 13.09.1915
 // TODO: GET BETTER RT. CODE ITSELF WORKS, BUT I KEEP GETTING RUNTIME ERROR ON HACKERRANK.
