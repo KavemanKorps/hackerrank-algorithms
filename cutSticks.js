64c0b11277dca855db5ad17adba1ac9c795f8377
roman <ronnoverro@gmail.com>
2023-11-18 00:26:34 -0800

    finally done with cutsticks

--- cutSticks.js
+++ cutSticks.js
@@ -1,27 +1,26 @@
 function cutTheSticks(arr) {
     // Write your code here
-    let arrCopy = [...arr];
     let junkArr = [];
     let currJunk = 0;
-    // need to loop over array y amount of times (biggest num in arr)
-    // instead of removing them altogether, why not just make them 0?
-    while (true) {
-        let shortest = Math.min(...arrCopy);
-        for (let i = 0; i < arrCopy.length; i++) {
-            if (arrCopy[i] == 0) {
-                arrCopy.splice(i, 1);
+    while (arr.length) {
+        // console.log(arr);
+        let shortest = Math.min(...arr);
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] -= shortest;
+            currJunk++;
+            if (arr[i] <= 0) {
+                arr.splice(i, 1);
                 i--;
             }
-            else {
-                arrCopy[i] -= shortest;
-                currJunk++;
-            }
         }
-        //console.log(arrCopy);
         junkArr.push(currJunk);
-        if (arrCopy.length <= 0) break;
+        currJunk = 0;
     }
     console.log(junkArr);
-}
+};
 
-cutTheSticks([1, 2, 3]);
+cutTheSticks([1, 2, 3]);    // [3, 2, 1]....[1, 2, 3] -> [1, 2] (3) -> [1] (2) -> [] (1)
+// [1, 2, 3]
+// [0, 1, 2]
+// [0, 0, 1]
+// [0, 0, 0]
