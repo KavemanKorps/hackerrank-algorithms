f9e094fdf17acd2a4170b22b889b5185ea4bf036
roman <ronnoverro@gmail.com>
2023-11-21 00:17:49 -0800

    jumpClouds done. Used chatgpt. Wouldn't have ever been able to solve on my own

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -1,13 +1,17 @@
+// YES, this is doable without recursion. Hint: use a do-while loop.
+
 function jumpingOnClouds(c, k) {
+    const n = c.length;
     let energy = 100;
-    let n = c.length;
-    for (let i = 0; i < c.length; i += k) {
-        console.log(i);
-        if (c[(i + k) % n] == 0) energy -= 1;
-        else energy -= 3;
-    }
+    let i = 0;
+  
+    do {
+      i = (i + k) % n;
+      energy -= 1 + 2 * c[i];
+    } while (i !== 0);
+  
     console.log(energy);
 }
 
-// jumpingOnClouds([0, 0, 1, 0], 2);   // CORRECT. Only iterates the i values 0 and 2.
-jumpingOnClouds([1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3); // expected: 80. Returns 94.
+// jumpingOnClouds([0, 0, 1, 0], 2);                    // CORRECT. Only iterates the i values 0 and 2.
+jumpingOnClouds([1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3);     // expected: 80. Returns 94.
