5155ba7af74a4e922514c99b711ee5bcbe02c440
roman <ronnoverro@gmail.com>
2023-11-23 12:14:48 -0800

    repeat correct but not efficient enough

--- repeatingString.js
+++ repeatingString.js
@@ -1,18 +1,18 @@
-function repeatingString(s, n) {
-    let newArr = [];
-    for (let i = 0; i < n; i % s.length) {
-        // if (newArr.length < n && s[i] == "a") newArr.push(s[i]);
-        if (newArr.length < n) {
-            newArr.push(s[i]);
-            // if (s[i] == "a") num++;
-            i++;
+// I could add a base case for if the array is entirely one letter
+function repeatedString(s, n) {
+    // BASE CASE:
+    if (s.length == 1) console.log(n);
+    else {
+        let newArr = [];
+        for (let i = 0; i < n; i++) {
+            let temp = s[i % s.length]; 
+            if (temp == "a") newArr.push(temp);
         }
-        else break;
+        console.log(newArr.length);
     }
-    
-    // console.log(newArr.filter(a => a == "a").length);
-    console.log(newArr);
 }
 
-repeatingString("abcac", 10);   // abcacabcac
-repeatingString("aba", 10);     // abaabaabaa, 8 (should be 7)
+repeatedString("abcac", 10);   // abcacabcac
+repeatedString("aba", 10);     // abaabaabaa, 8 (should be 7)
+repeatedString("a", 1000000000000);    // 1000000000000
+
