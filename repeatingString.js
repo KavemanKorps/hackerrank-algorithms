aa71579eccef2240c1f8133afdce6df2e84642d9
roman <ronnoverro@gmail.com>
2023-11-23 17:29:21 -0800

    equalizeArray in progress

--- repeatingString.js
+++ repeatingString.js
@@ -1,18 +1,25 @@
-// I could add a base case for if the array is entirely one letter
 function repeatedString(s, n) {
     // BASE CASE:
-    if (s.length == 1) console.log(n);
+    if (s.length == 1) return n;
     else {
-        let newArr = [];
+        let count = 0;
         for (let i = 0; i < n; i++) {
-            let temp = s[i % s.length]; 
-            if (temp == "a") newArr.push(temp);
+            if (s[i % s.length] == "a") count++;
         }
-        console.log(newArr.length);
+        console.log(count);
     }
 }
+// repeatedString("abcac", 10);   // THIS IS ONE HUNDRED MILLION (100,000,000) abcacabcac.
+// repeatedString("aba", 10);     // abaabaabaa, 8 (should be 7)
+// repeatedString("cfimaakj", 554045874191);    // 554,045,874,191
+// cfimaakjcfimaakjcfimaakjcfimaakjcfimaakjcfimaakjcfimaakjcfimaakjcfimaakjcfimaakjcfimaakjcfimaakj......
 
-repeatedString("abcac", 10);   // abcacabcac
-repeatedString("aba", 10);     // abaabaabaa, 8 (should be 7)
-repeatedString("a", 1000000000000);    // 1000000000000
+function hola(str) {
+    str = str.map(a => a = "$");
+    console.log(str);
+}
 
+hola(["j", "o", "e"]);
+
+
+// fuck
