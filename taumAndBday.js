7f9544e92b1c47097d0f102b366f24547e46dbf1
roman <ronnoverro@gmail.com>
2023-11-26 23:51:49 -0800

    taumAndBday done

--- taumAndBday.js
+++ taumAndBday.js
@@ -1,17 +1,22 @@
 function taumBday(b, w, bc, wc, z) {
-    // baseCost = (b * bc) + (w * wc);
-    let baseCost, zCost;
-    let cheapQuant, expensiveQuant;
-    if (bc == wc) {
-        console.log((b * bc) + (w * wc));
-    } else {
-        expensiveQuant = bc > wc ? b : w;
-        let cheaper = bc > wc ? wc : bc;        // PRICE 
+    b = BigInt(b);
+    w = BigInt(w);
+    bc = BigInt(bc);
+    wc = BigInt(wc);
+    z = BigInt(z);
 
-        zCost = cheaper * (b + w) + (expensiveQuant * z);
-        console.log(zCost);
-    }
+    let baseCost = (b * bc) + (w * wc);
+
+    let expensiveQuant = bc > wc ? b : w;
+    let cheaper = bc > wc ? wc : bc;        // PRICE 
+    let zCost = (cheaper * (b + w)) + (expensiveQuant * z);
+
+    console.log(parseInt(baseCost));
+    console.log(parseInt(zCost));
+    // console.log(zCost > baseCost ? baseCost : zCost);
 }
 
-taumBday(3, 6, 9, 1, 1);
+taumBday(3, 6, 9, 1, 1);    // 12. good
+taumBday(5, 9, 2, 3, 4);    // should return 37
+taumBday(742407782, 90529439, 847666641, 8651519, 821801924);   // in this case, zCost needs to be returned.
 
