9999e80e98244380d7fbb098275224f417a0e1ea
roman <ronnoverro@gmail.com>
2023-12-19 15:08:35 -0800

    savePrisoner done

--- savePrisoner4.js
+++ savePrisoner4.js
@@ -16,4 +16,4 @@ console.log(saveThePrisoner(5, 2, 1));   // -> 1, 2....RETURN 2
 
 console.log(saveThePrisoner(5, 2, 2));   // -> 2, 3....RETURN 3
 
-console.log(saveThePrisoner(999999999, 999999998, 999983945));  // -> 999983943
+console.log(saveThePrisoner(999999999, 999999998, 999983945));  // -> 999983943  
--- savePrisoner5.js
+++ savePrisoner5.js
@@ -1,5 +1,24 @@
-function saveThePrisoner(n, m, s) {
-    let curr =   
+function saveThePrisoner(dudes, candy, s) {
+    let extend = s + candy - 1
+    
+    if (extend > dudes) {
+        let remaining = extend - dudes;
+        while (remaining > dudes) {
+            remaining -= dudes;
+        }
+        return remaining;
+    } else return s + candy - 1;
 }
 
-console.log(saveThePrisoner(4, 6, 2));
+// n, m, s
+// console.log(saveThePrisoner(3, 7, 3));    // -> 3
+
+// console.log(saveThePrisoner(7, 19, 2));   // -> 6
+
+// console.log(saveThePrisoner(5, 3, 4));    // -> 1  
+
+console.log(saveThePrisoner(5, 2, 1));   // -> 1, 2....RETURN 2
+
+console.log(saveThePrisoner(5, 2, 2));   // -> 2, 3....RETURN 3
+
+// console.log(saveThePrisoner(999999999, 999999998, 999983945));  // -> 999983943  
