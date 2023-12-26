e5214550c679dabe79a822d6f2f543e76b155730
roman <ronnoverro@gmail.com>
2023-12-26 00:44:49 -0700

    more spaceStation progress

--- spaceStation2.js
+++ spaceStation2.js
@@ -1,17 +1,20 @@
 function flatlandSpaceStations(n, c) {
     let maxys = [];
-    let currStation = c[0];
-    let nextStation = c[1];
     for (let i = 0; i < n; i++) {
         let difference = Math.abs(c[0] - i);   
-        let otherDiff = c[1] ? Math.abs(c[1] - i) : false;  
+        let otherDiff = c[1] ? Math.abs(c[1] - i) : difference + 1;  // difference + 1 ensures first value is less 
+        // let otherDiff = Math.abs(c[1] - i);
         
         maxys.push(Math.min(difference, otherDiff));
         
-        if (difference == otherDiff) c.shift();
+        if (difference == otherDiff || difference > otherDiff) c.shift();
 
-        console.log(difference, otherDiff, c);
+        // console.log(difference, otherDiff, c);
     }
+    console.log(Math.max(...maxys));
+    // console.log(maxys);
 };
 
-flatlandSpaceStations(5, [0, 4]);
+flatlandSpaceStations(5, [0, 4]);            // -> 0, 1, 2, 1, 0.....2
+flatlandSpaceStations(10, [0, 5, 8]);           // -> 2 
+flatlandSpaceStations(6, [0, 1, 2, 3, 4, 5]);   // -> 0 
