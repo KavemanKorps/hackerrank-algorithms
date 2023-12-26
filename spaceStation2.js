0de6572f66c4447680f3ac6846558cd12fa0617b
roman <ronnoverro@gmail.com>
2023-12-26 14:45:01 -0700

    using variable instead of array for max

--- spaceStation2.js
+++ spaceStation2.js
@@ -2,19 +2,20 @@ function flatlandSpaceStations(n, c) {
     let maxys = [];
     for (let i = 0; i < n; i++) {
         let difference = Math.abs(c[0] - i);   
-        let otherDiff = c[1] ? Math.abs(c[1] - i) : difference + 1;  // difference + 1 ensures first value is less 
-        // let otherDiff = Math.abs(c[1] - i);
+        let otherDiff = c[1] ? Math.abs(c[1] - i) : difference + 1; 
+
+        if (difference == otherDiff || difference > otherDiff) c.shift();
         
         maxys.push(Math.min(difference, otherDiff));
-        
-        if (difference == otherDiff || difference > otherDiff) c.shift();
 
-        // console.log(difference, otherDiff, c);
+        console.log(difference, otherDiff, c);
     }
-    console.log(Math.max(...maxys));
+    // return Math.max(...maxys);
     // console.log(maxys);
 };
 
-flatlandSpaceStations(5, [0, 4]);            // -> 0, 1, 2, 1, 0.....2
-flatlandSpaceStations(10, [0, 5, 8]);           // -> 2 
-flatlandSpaceStations(6, [0, 1, 2, 3, 4, 5]);   // -> 0 
+// flatlandSpaceStations(5, [0, 4]);            // -> 0, 1, 2, 1, 0.....2
+// flatlandSpaceStations(10, [0, 5, 8]);           // -> 2 
+// console.log(flatlandSpaceStations(6, [0, 1, 2, 3, 4, 5]));   // -> 0 
+
+flatlandSpaceStations(10, [0, 5, 8]);
