b49b75dec9486d5b84771975ef19f68b86e0bedd
roman <ronnoverro@gmail.com>
2023-12-27 23:32:18 -0800

    spacestation so close

--- spaceStation4.js
+++ spaceStation4.js
@@ -2,6 +2,7 @@ function flatlandSpaceStations(n, c) {
     c.unshift(0);
     c.push(n - 1);
     let newArr = [...new Set(c)].sort((a, b) => a - b);
+    
     let distances = [];
 
     for (let i = 0; i < newArr.length - 1; i++) {
@@ -10,17 +11,17 @@ function flatlandSpaceStations(n, c) {
 
     let sucker = Math.max(...distances); 
 
-    // -2 because we add 2 values:
-    if (c.length - 2 == n) return 0;
-    else if (sucker == distances[distances.length - 1]) return sucker; 
-    else return Math.floor(Math.max(...distances) / 2);
+    return newArr;
+    // return distances;
 };
 
 // flatlandSpaceStations(5, [0, 4]);            // -> 0, 1, 2, 1, 0.....2
 
-// flatlandSpaceStations(11, [0, 5, 8]);                           // -> 2 
-console.log(flatlandSpaceStations(99992, [90467, 18058, 99109, 48463]));     // -> should return 21002. 51528 instead.
-console.log(flatlandSpaceStations(11, [0, 5, 8]));   // -> 2
-console.log(flatlandSpaceStations(9, [2, 5]));       // -> 3 
-console.log(flatlandSpaceStations(6, [0, 1, 2, 3, 4, 5]));      // -> 0
-console.log(flatlandSpaceStations(5, [0, 4]));      // -> 2
+console.log(flatlandSpaceStations(99992, [90467, 18058, 99109, 48463]));     // -> 21002
+console.log(flatlandSpaceStations(11, [0, 5, 8]));                           // -> 2
+console.log(flatlandSpaceStations(9, [2, 5]));                               // -> 3 
+console.log(flatlandSpaceStations(6, [0, 1, 2, 3, 4, 5]));                   // -> 0
+console.log(flatlandSpaceStations(5, [0, 4]));                               // -> 2
+console.log(flatlandSpaceStations(99989, [75453, 36129, 64502, 46817]));     // -> 36129
+console.log(flatlandSpaceStations(100, [99]));                               // -> 99
+console.log(flatlandSpaceStations(90, [4, 76, 16, 71, 56, 7, 77, 31, 2, 66, 12, 32, 57, 11, 19, 14, 42]));  // -> 12
