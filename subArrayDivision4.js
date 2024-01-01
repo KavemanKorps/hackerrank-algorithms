39c46ef064691aaee84f64c1f80ebe25393cc051
roman <ronnoverro@gmail.com>
2024-01-01 14:56:17 -0800

    subArray more progress

--- subArrayDivision4.js
+++ subArrayDivision4.js
@@ -1,21 +1,37 @@
 function birthday(s, d, m) {
-    let currArr = [];
-    let count = 0;
+    let count = [];
+    let total = 0;
     for (let i = 0; i < s.length; i++) {
-        let sum = currArr.length ? currArr.reduce((a, b) => a + b) : 0;
-        if (sum < d && currArr.length < m) {
-            currArr.push(s[i]);
-            s = s.splice(i, 1);
+        // console.log(count, s, i);
+
+        let sum = count.length ? count.reduce((a, b) => a + b) : 0;
+
+        if (sum == d) {
+            total++;
+            count = [];
+            i--;
         }
-        else if (sum > d) currArr.pop();
-        else if (sum == d) {
-            count++;
-            currArr = [];
-        };
-        console.log(currArr, s);
+
+        else if (sum > d) {
+            s.push(count.pop());
+            i--;
+        }
+
+        // else if (s.length < m) break;
+
+        else if (sum < d && count.length < m) {
+            count.push(s[i]);
+            s.splice(i, 1);
+            i--;
+        }
     }
-    // return count;
+    return total;
+    // console.log(total);
 }
 
-// console.log(birthday([2, 2, 1, 3, 2], 4, 2));
-birthday([2, 2, 1, 3, 2], 4, 2);
+// console.log(birthday([2, 2, 1, 3, 2], 4, 2));       // 2
+// birthday([2, 2, 1, 3, 2], 4, 2);
+
+console.log(birthday([1, 2, 1, 3, 2], 3, 2));       // 2
+// birthday([1, 2, 1, 3, 2], 3, 2);       // 2
+// s, d, m
--- testingShit.js
+++ testingShit.js
@@ -1,9 +1,12 @@
-let stupid2 = [1, 2, 3, 4, 5, 6];
+// WORKS GOOD
+let stupid = [1, 2, 3, 2, 5, 6, 5, 7, 2, 3, 1, 2];
 function birthday(s, d, m) {
-    for (let i = 0; i < stupid2.length; i = 0) {
-        for (let y = i + 1; y < stupid2.length; y++) {
-            console.log(stupid2[i] + stupid2[y]);
+    for (let i = 0; i < s.length; i++) {
+        if (stupid[i] <= 3) {
+            stupid.splice(i, 1);
+            i--;
         }
-        break;
-    };
+        console.log(stupid);
+    }
 }
+birthday([2, 2, 1, 3, 2], 4, 2);
