2ac5631c78fbab0c03911a76ece13cb05f6b3558
roman <ronnoverro@gmail.com>
2024-02-01 17:47:14 -0800

    fuck

--- 3dSurfaceArea4.js
+++ 3dSurfaceArea4.js
@@ -8,7 +8,7 @@ function surfaceArea(A) {
     }
 
     for (let row = 0; row < A.length; row++) {
-        curr += A[row].length * 2;        // Bottom and Top 
+        curr += A[row].length * 2;        // Bottom and Top
         curr += Math.max(...A[row]) * 2;  // the sides 
 
         // starting off with "front" face:
@@ -23,13 +23,7 @@ function surfaceArea(A) {
             curr += blockValue(currBlock, next);
 
             // FACES COMPLETE. NOW FOR THE INNER FACES.
-            let left = block - 1 >= 0 ? A[row][block - 1] : 0;
-            let right = block + 1 < A[row].length ? A[row][block + 1] : 0;
-
-            if (currBlock < left && currBlock < right) {
-                curr += left - currBlock;
-                curr += right - currBlock;
-            }
+            
         }
     }
     return curr;
@@ -40,31 +34,3 @@ console.log(surfaceArea(
     [91, 80, 7, 41, 36, 11, 48, 57, 40, 43]
     ]
 ));     // 1276. 23 extra ones being added (1299)
-
-// GOOD
-// console.log(surfaceArea(
-//     [
-//     [2, 1, 3]
-//     ]
-// ));     // 1276. 23 extra ones being added (1299)
-
-
-// console.log(surfaceArea(
-//     [[1, 3, 4],
-//      [2, 2, 3],
-//      [1, 2, 4]
-//     ]
-// ));       // should return 60.
-
-// console.log(surfaceArea(
-//     [
-//      [1, 2],
-//      [3, 2]
-//     ]));        // 28
-
-// console.log(surfaceArea(
-//     [[3, 1, 2],
-//      [1, 1, 1],
-//      [1, 2, 1]
-//     ]
-// ));       // should return 47
